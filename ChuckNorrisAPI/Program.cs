f087948d97260afb345044d3739bc0c982711cf1
Jeremy <jshudd@gmail.com>
2020-12-16 14:33:50 -0600

    finished project

---
+++ ChuckNorrisAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ChuckNorrisAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitString = "";
+
+            do
+            {
+                NorrisQuote();
+
+                Console.WriteLine();
+                Console.WriteLine("Type 'exit' to quit, type anything to continue");
+
+                exitString = Console.ReadLine();
+
+            } while (exitString.ToLower() != "exit");
+            
+        }
+
+        public static void NorrisQuote()
+        {
+            var norrisURL = "https://api.chucknorris.io/jokes/random";
+
+            var client = new HttpClient();
+
+            var norrisResponse = client.GetStringAsync(norrisURL).Result;
+
+            var norrisQuote = JObject.Parse(norrisResponse).GetValue("value").ToString();
+
+            Console.WriteLine($"{norrisQuote}");
+        }
+    }
+}
